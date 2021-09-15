a16a4c4726c4c76fff2b888a6910768d77d84c29
jeffgonzalez <jeff_gonzalez@progressive.com>
2021-09-15 16:08:43 -0400

    fixed that ignored test

--- src/app/components/shopping-list-item/shopping-list-item.component.spec.ts
+++ src/app/components/shopping-list-item/shopping-list-item.component.spec.ts
@@ -1,6 +1,10 @@
 import { Component } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Store } from "@ngrx/store";
+import { provideMockStore } from "@ngrx/store/testing";
 import { ShoppingListItemModel } from "src/app/models";
+import { AppState } from "src/app/reducers";
+import { ShoppingEntity, ShoppingState } from "src/app/reducers/shopping-list.reducer";
 import { ShoppingListItemComponent } from "./shopping-list-item.component";
 
 describe('ShoppingListItemComponent', () => {
@@ -9,6 +13,7 @@ describe('ShoppingListItemComponent', () => {
 
   describe('Unpurchased Item With No Store', () => {
     let testHostFixture: ComponentFixture<UnPurchasedTestHostComponent>;
+    let store: Store<AppState>;
     @Component({
       template: `<app-shopping-list-item [item]="item"></app-shopping-list-item>`
     })
@@ -21,11 +26,16 @@ describe('ShoppingListItemComponent', () => {
     }
 
     beforeEach(() => {
+      const initialState: ShoppingState = {
+        ids: [],
+        entities: {}
+      }
       TestBed.configureTestingModule({
-        declarations: [UnPurchasedTestHostComponent, ShoppingListItemComponent]
+        declarations: [UnPurchasedTestHostComponent, ShoppingListItemComponent],
+        providers: [provideMockStore({ initialState })]
       });
       testHostFixture = TestBed.createComponent(UnPurchasedTestHostComponent);
-
+      store = TestBed.inject(Store);
     });
 
     it('has the right description', () => {
@@ -37,6 +47,25 @@ describe('ShoppingListItemComponent', () => {
       testHostFixture.detectChanges();
       expect(testHostFixture.nativeElement.querySelector('[data-shopping-list-item-purchased]')).not.toBeNull();
       expect(testHostFixture.nativeElement.querySelector('[data-shopping-list-item-purchased] [data-shopping-list-item-purchase-button]')).not.toBeNull();
+    });
+
+    it('dispatches the action to the store when the purchase button is clicked', () => {
+      const dispatchSpy = spyOn(store, 'dispatch')
+      testHostFixture.detectChanges();
+      const button = testHostFixture.nativeElement.querySelector('[data-shopping-list-item-purchased] [data-shopping-list-item-purchase-button]');
+      //expect(button).not.toBeNull();
+      button.click();
+
+      expect(dispatchSpy.calls.count()).toBe(1);
+      const actionDispatched = dispatchSpy.calls.first().args[0] as { type: string, payload: ShoppingEntity };
+      expect(actionDispatched.payload).toEqual({
+        id: '1',
+        description: 'Tacos',
+        purchased: false
+      });
+      expect(actionDispatched.type).toEqual('[shopping-list] mark item purchased');
+
+
     });
 
 
@@ -57,8 +86,13 @@ describe('ShoppingListItemComponent', () => {
     }
 
     beforeEach(() => {
+      const initialState: ShoppingState = {
+        ids: [],
+        entities: {}
+      }
       TestBed.configureTestingModule({
-        declarations: [TestHostComponent, ShoppingListItemComponent]
+        declarations: [TestHostComponent, ShoppingListItemComponent],
+        providers: [provideMockStore({ initialState })]
       });
       testHostFixture = TestBed.createComponent(TestHostComponent);
 
